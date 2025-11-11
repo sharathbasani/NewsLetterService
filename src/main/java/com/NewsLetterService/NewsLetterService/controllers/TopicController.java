e79d9cbc909272c93fadeb56e407c5e0a651d3e5
sharathbasani <sharathbasani24@gmail.com>
2025-11-12 02:00:11 +0530

    added CRUD api's for topic

--- src/main/java/com/NewsLetterService/NewsLetterService/controllers/TopicController.java
+++ src/main/java/com/NewsLetterService/NewsLetterService/controllers/TopicController.java
@@ -1,9 +1,45 @@
 package com.NewsLetterService.NewsLetterService.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.NewsLetterService.NewsLetterService.entities.Topic;
+import com.NewsLetterService.NewsLetterService.services.TopicService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topic")
 public class TopicController{
+
+    @Autowired
+    private TopicService topicService;
+
+    @PostMapping("/createTopic")
+    public ResponseEntity<Topic> createTopic(@NotNull Topic topic) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(topicService.createTopic(topic));
+    }
+
+    @GetMapping("/getAllTopics")
+    public ResponseEntity<List<Topic>> getAllTopics() {
+        return ResponseEntity.ok(topicService.getAllTopics());
+    }
+
+    @GetMapping("/getTopic/{id}")
+    public ResponseEntity<Topic> getTopicById(@PathVariable Long id) {
+        return ResponseEntity.ok(topicService.getTopicById(id));
+    }
+
+    @PatchMapping("/updateTopic/{id}")
+    public ResponseEntity<Topic> updateTopic(@PathVariable Long id, @NotNull Topic topic) {
+        return ResponseEntity.ok(topicService.updateTopic(id, topic));
+    }
+
+    @DeleteMapping("/deleteTopic/{id}")
+    public ResponseEntity<Void> deleteTopic(@PathVariable Long id) {
+        topicService.deleteTopic(id);
+        return ResponseEntity.noContent().build();
+    }
 }
---
+++ src/main/java/com/NewsLetterService/NewsLetterService/services/TopicService.java
@@ -0,0 +1,42 @@
+package com.NewsLetterService.NewsLetterService.services;
+
+import com.NewsLetterService.NewsLetterService.customExceptions.topicExceptions.TopicNotFoundException;
+import com.NewsLetterService.NewsLetterService.entities.Topic;
+import com.NewsLetterService.NewsLetterService.repositories.TopicRepo;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    TopicRepo topicRepo;
+
+    public Topic createTopic(@NotNull Topic topic) {
+        return topicRepo.save(topic);
+    }
+
+    public List<Topic> getAllTopics() {
+        return topicRepo.findAll();
+    }
+
+    public Topic getTopicById(Long id) {
+        return topicRepo.findById(id).orElseThrow(() -> new TopicNotFoundException(id));
+    }
+
+    public Topic updateTopic(Long id, @NotNull Topic topic) {
+        Topic existingTopic = getTopicById(id);
+        existingTopic.setName(topic.getName());
+        existingTopic.setDescription(topic.getDescription());
+        return topicRepo.save(existingTopic);
+    }
+
+    public void deleteTopic(Long id) {
+        Topic topic = getTopicById(id);
+        topicRepo.delete(topic);
+    }
+}
+
